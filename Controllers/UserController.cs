b3c5b0f5816d2515e07764632be2b437befe16c2
doo <gostbaducking1@nate.com>
2025-08-12 00:14:03 +0900

    fix: user controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using rest1.Models;
 using rest1.Services;
+using System.Text.Json.Serialization;
 
 namespace rest1.Controllers
 {
     [ApiController]
-    [Route("api/v1/user/[controller]")]
+    [Route("api/v1/user/")]
     public class UserController : Controller
     {
         private readonly IUserService _userService;
@@ -14,11 +16,40 @@ namespace rest1.Controllers
             _userService = userService;
         }
 
-        [HttpPost("userList")]
-        public async Task<IActionResult> getChatList()
+        [HttpGet("list")]
+        public async Task<IActionResult> getUserList()
         {
             var userList = _userService.getUserList();
             return Ok(userList);
         }
+
+        [HttpGet("{usrNo}")]
+        public async Task<IActionResult> getUser(int usrNo)
+        {
+            var user = _userService.getUser(usrNo);
+            return Ok(user);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> create([FromBody] User user)
+        {
+            _userService.save(user);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> login([FromBody] UserLoginRequestDto dto)
+        {
+            var user = _userService.login(dto.UsrId, dto.Password);
+            return Ok(user);
+        }
+    }
+
+    public class UserLoginRequestDto
+    {
+        [JsonPropertyName("usrId")]
+        public String UsrId { get; set; }
+        [JsonPropertyName("password")]
+        public String Password { get; set; }
     }
 }
