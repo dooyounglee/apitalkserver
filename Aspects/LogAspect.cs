d107444b8d9b6462bf77bf23341fd0aa3ed1745e
doo <gostbaducking1@nate.com>
2025-08-23 22:34:29 +0900

    feat: log aop 추가

---
+++ Aspects/LogAspect.cs
@@ -0,0 +1,23 @@
+﻿using AspectInjector.Broker;
+using OTILib.Util;
+
+namespace rest1.Aspects
+{
+    [Aspect(Scope.Global)]
+    public class LogAspect
+    {
+        [Advice(Kind.Before, Targets = Target.Method)]
+        public void LogEnter([Argument(Source.Name)] string methodName)
+        {
+            Console.WriteLine($"--> Entering method: {methodName}");
+            OtiLogger.log1($"--> Entering method: {methodName}");
+        }
+
+        [Advice(Kind.After, Targets = Target.Method)]
+        public void LogExit([Argument(Source.Name)] string methodName)
+        {
+            Console.WriteLine($"<-- Exiting method: {methodName}");
+            OtiLogger.log1($"<-- Exiting method: {methodName}");
+        }
+    }
+}
---
+++ Attibutes/LogAttribute.cs
@@ -0,0 +1,16 @@
+﻿using AspectInjector.Broker;
+using rest1.Aspects;
+
+namespace rest1.Attibutes
+{
+    /*
+     * method위에 [Log]를 붙이면 method 시작과 끝에 log찍힘
+     * 왜 "Log"냐면 Attribute가 "Log"Attribute라서.
+     * LogAttrbiute일 경우 [LogAttrbiute]로 써야함.
+     */
+    [Injection(typeof(LogAspect))]
+    [AttributeUsage(AttributeTargets.Method)]
+    public class LogAttribute : Attribute
+    {
+    }
+}
